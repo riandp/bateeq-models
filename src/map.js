6879e4603dd50d41318f46a3714d4e4123961f90
Tris Setiawan <tris.setiawan@yahoo.com>
2016-06-24 09:31:32 +0000

    1.0.0
    add map

---
+++ src/map.js
@@ -0,0 +1,16 @@
+module.exports = {
+    article: {
+        Article: 'articles',
+        ArticleApproval: 'article-approvals'
+    },
+    inventory: {
+        Storage: 'storages',
+        StockIn: 'stock-ins',
+        StockOut: 'stock-outs',
+        Inventory: 'inventories',
+        InventoryMovement: 'inventory-movements'
+    },
+    manufacture: {
+        Finishing: 'finishings'
+    }
+}
