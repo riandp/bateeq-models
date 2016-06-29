4130738138bc3c49c4e6d354108fba97476576b1
Tris Setiawan <tris.setiawan@yahoo.com>
2016-06-29 05:16:38 +0000

    1.0.0

--- test/validator.js
+++ test/validator.js
@@ -219,6 +219,12 @@ var validateArticleVariant = function(data) {
     data.should.have.property('code');
     data.code.should.instanceof(String);
 
+    data.should.have.property('name');
+    data.name.should.instanceof(String);
+
+    data.should.have.property('description');
+    data.description.should.instanceof(String);
+
     data.should.have.property('size');
     data.size.should.instanceof(String);
 
