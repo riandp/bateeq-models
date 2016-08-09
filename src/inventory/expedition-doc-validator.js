9fd4ee815ba55511ed7bb35889ac17492a34650a
lioenel <kepin91@yahoo.co.id>
2016-08-09 15:44:17 +0700

    update unit test
    update unit test

--- src/inventory/expedition-doc-validator.js
+++ src/inventory/expedition-doc-validator.js
@@ -1,18 +1,34 @@
 require("should");
+var validateTransferOut = require("./transfer-out-doc-validator");
+var validateExpeditionSpk = require("./expedition-spk-validator");
 
 module.exports = function(data) {
     data.should.not.equal(null);
     data.should.instanceof(Object);
 
-    data.should.have.property('code');
+   data.should.have.property('code');
     data.code.should.instanceof(String);
 
-    data.should.have.property('name');
-    data.name.should.instanceof(String);
+    data.should.have.property('date');
+    data.date.should.instanceof(Date);
 
-    data.should.have.property('description');
-    data.description.should.instanceof(String);
+    data.should.have.property('expedition');
+    data.expedition.should.instanceof(String);
+    
+    data.should.have.property('weight');
+    data.weight.should.instanceof(String); 
+    
+    data.should.have.property('transferOutDocuments');
+    data.transferOutDocuments.should.instanceof(Array);
 
-    data.should.have.property('config');
-    data.config.should.instanceof(Object);
+    data.should.have.property('spkDocuments');
+    data.spkDocuments.should.instanceof(Array);
+
+    for (var transferOutDocument of data.transferOutDocuments) {
+        validateTransferOut(transferOutDocument);
+    } 
+    
+    for (var spkDocument of data.spkDocuments) {
+        validateExpeditionSpk(spkDocument);
+    } 
 };
--- src/inventory/expedition-spk-validator.js
+++ src/inventory/expedition-spk-validator.js
@@ -4,15 +4,15 @@ module.exports = function(data) {
     data.should.not.equal(null);
     data.should.instanceof(Object);
 
-    data.should.have.property('code');
-    data.code.should.instanceof(String);
+    data.should.have.property('spkDocumentId');
+    data.spkDocumentId.should.instanceof(Object);
 
-    data.should.have.property('name');
-    data.name.should.instanceof(String);
+    data.should.have.property('spkDocument');
+    data.spkDocument.should.instanceof(Object);
 
-    data.should.have.property('description');
-    data.description.should.instanceof(String);
+    data.should.have.property('quantity');
+    data.quantity.should.be.Number();
 
-    data.should.have.property('config');
-    data.config.should.instanceof(Object);
+    data.should.have.property('remark');
+    data.remark.should.instanceof(String); 
 };
